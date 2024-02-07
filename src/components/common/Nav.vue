291806b972c6d688c110e9a002d46b3e34053c2a
Bharath N <109462055+Bharathnbs@users.noreply.github.com>
2024-02-07 17:42:46 +0530

    nav - creation

--- src/components/common/Nav.vue
+++ src/components/common/Nav.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-        <div class="w-100 h-14 bg-slate-300">
-            <img class="h-10" src="../../assets/logo/brand_logo.png" alt="Brand Logo">
-            <div>
-                <ul class="flex">
-                    <li><router-link :to="{name: 'LandingPage'}">Home</router-link></li>
-                    <li><router-link :to="{name: 'ProductsPage'}">Products</router-link></li>
+        <div class="flex bg-white w-100 h-14">
+
+            <div class="md:w-48 md:h-full">
+                <img class="h-10 my-2" src="../../assets/logo/brand_logo.png" alt="Brand Logo">
+            </div>
+            <div class="hidden md:inline-block md:w-full md:h-full">
+                <ul class="flex justify-center my-4">
+                    <li class="mx-8"><router-link :to="{name: 'LandingPage'}">Home</router-link></li>
+                    <li class="mx-8"><router-link :to="{name: 'ProductsPage'}">Products</router-link></li>
+                    <li class="mx-8"><router-link :to="{name: 'ProductsPage'}">Products</router-link></li>
+                    <li class="mx-8"><router-link :to="{name: 'ProductsPage'}">Products</router-link></li>
+                    <li class="mx-8"><router-link :to="{name: 'ProductsPage'}">Products</router-link></li>
+                    <li class="mx-8"><router-link :to="{name: 'ProductsPage'}">Products</router-link></li>
                 </ul>
             </div>
         </div>
